3780ef942ac74807d046affabd40dece5fd3e331
kaniis <sd130685@W1202185.ssc.local>
2016-02-12 12:29:45 +0100

    new functions

--- application/randomGame.php
+++ application/randomGame.php
@@ -3,13 +3,45 @@ require '../classes/mysql.php';
 $mysql = new database();
 $mysql->connectDb();
 
-$civ1 = rand(1, 2);
-$civ2 = rand(1, 2);
-while($civ2 === $civ1)
+function randDupCiv()
 {
-	$civ2 = rand(1,2);
+	$mapSize = [2, 4, 6, 8, 10, 14];
+	$size = $mapSize[rand(0, 5)];
+	$civs = [];
+	for($i = 0; $i < $size; $i++)
+	{
+
+		$civs[] = rand(1, 14);
+
+	}
+	return $civs;
 }
-$civs = array($civ1, $civ2);
+
+function randUniCiv($db)
+{
+	$mapSize = [2, 4, 6, 8, 10, 14];
+	$size = $mapSize[rand(0, 5)];
+	$civList = [];
+	$civRows = $db->fetchRows('civilization', 'id');
+	while($civ = $civRows->fetch_assoc())
+	{
+		$civList[] = $civ['id'];
+	}
+
+	$civs = [];
+	for($i = 0; $i < $size; $i++)
+	{
+		$civId = rand(0, count($civList) - 1);
+		$civs[] = $civList[$civId];
+		unset($civList[$civId]);
+		$civList = array_values($civList);
+	}
+
+	return $civs;
+}
+
+$civs = randUniCiv($mysql);
+
 $player = 1;
 
 foreach($civs as $key => $value)
@@ -20,15 +52,15 @@ foreach($civs as $key => $value)
 	$player++;
 }
 
-$victories = array('time', 'domination', 'science', 'diplomatic', 'culture');
+$victories = ['time', 'domination', 'science', 'diplomatic', 'culture'];
 echo 'Victory type: ', $victories[rand(0,4)], ' victory<br/>';
 
-$difficulties = array('settler', 'chieftain', 'warlord', 'prince', 'king', 'emperor', 'immortal', 'deity');
+$difficulties = ['settler', 'chieftain', 'warlord', 'prince', 'king', 'emperor', 'immortal', 'deity'];
 echo 'Difficulty level: ', $difficulties[rand(0,7)], '<br/>';
 
-$maps = array('Earth', 'continents', 'pangea', 'archipelago', 'fractal');
+$maps = ['Earth', 'continents', 'pangea', 'archipelago', 'fractal'];
 echo 'Map type: ', $maps[rand(0,4)], '<br/>';
 
-$paces = array('quick', 'standard', 'epic', 'marathon');
+$paces = ['quick', 'standard', 'epic', 'marathon'];
 echo 'Game pace: ', $paces[rand(0,3)];
 ?>
