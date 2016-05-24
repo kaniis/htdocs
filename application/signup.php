b1a96cfa66d685447a3a8f1369398771ea6325f9
kaniis <sd130685@W1202185.ssc.local>
2016-05-24 16:00:47 +0200

    worked on signup

--- application/signup.php
+++ application/signup.php
@@ -1,6 +1,5 @@
 <form method="post" action="signup.php">
 Username: <input type="text" name="username"/></br>
-E-mail: <input type="email" name="mail"/></br>
 Password: <input type="password" name="pass1"/></br>
 Repeat password: <input type="password" name="pass2"/></br>
 <input type="submit" name="signup"/>
@@ -16,7 +15,18 @@ if(isset($_POST['signup']))
 	$userRows = $mysql->fetchRows('users', 'id', 'name', $_POST['username']);
 	if($userRows->num_rows === 0)
 	{
-		echo 'not taken';
+		if($_POST['pass1'] === $_POST['pass2'])
+		{
+			$mysql->addUSer('users', 'id', 'name', $_POST['username']);
+		}
+		else
+		{
+			echo 'the passwords do not match!';
+		}
+	}
+	else
+	{
+		echo 'this username is already taken.';
 	}
 
 }
