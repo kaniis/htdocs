11b9e95a14a36642beee57b380b8353e5c3f84db
kaniis <sd130685@W1202185.ssc.local>
2016-06-14 15:47:22 +0200

    css works

---
+++ design.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="siteContainer">
+			<div class="navbar">
+				<p>Civ site</p>
+			</div>
+			<div class="background">
+				<p>Civ site</p>
+			</div>
+			<div class="mainBox">
+				<p>SAMPLE TEXT</p>
+			</div>
+		</div>
+		<div class="contentContainer">
+			<p class="content">Content</p>
+		</div>
+	</body>
+</html>
