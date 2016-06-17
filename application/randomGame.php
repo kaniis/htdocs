c98e70d4991c268aa85f1f8ddbe6aa452d41e3d0
kaniis <sd130685@W1202185.ssc.local>
2016-06-17 12:32:07 +0200

    works on random game options

--- application/randomGame.php
+++ application/randomGame.php
@@ -2,6 +2,8 @@
 require '../classes/mysql.php';
 database::connectDb();
 
+session_start();
+
 function randDupCiv()
 {
 	$mapSize = [2, 4, 6, 8, 10, 14];
@@ -39,26 +41,64 @@ function randUniCiv()
 	return $civs;
 }
 
-$civs = randUniCiv();
+?>
 
-$player = 1;
+<form method="post" action="/application/randomgame.php">
+	<select name="mode">
+		<option selected disabled>Select a mode</option>
+		<option value="0">Unique civs</option>
+		<option value="1">Allow duplicates</option>
+	</select>
+	<input type="submit" name="randomMode" value="Set">
+</form>
 
-foreach($civs as $key => $value)
+<?php
+
+if(isset($_POST['randomMode']))
 {
-	$results = database::fetchRows('civilization', 'name', 'id', $value);
-	$resultsRow = $results->fetch_row();
-	echo 'Player ', $player++, ': ', $resultsRow[0], '<br/>';
+	$_SESSION['mode'] = $_POST['mode'];
+	$_SESSION['randomMode'] = '';
 }
 
-$victories = ['time', 'domination', 'science', 'diplomatic', 'culture'];
-echo 'Victory type: ', $victories[rand(0,4)], ' victory<br/>';
+if(isset($_SESSION['randomMode']))
+{
+	$randomMode = array('only uniques', 'allow duplicates');
 
-$difficulties = ['settler', 'chieftain', 'warlord', 'prince', 'king', 'emperor', 'immortal', 'deity'];
-echo 'Difficulty level: ', $difficulties[rand(0,7)], '<br/>';
+	if($randomMode[$_SESSION['mode']] == 'only uniques')
+	{
+		$civs = randUniCiv();
+	}
+	elseif($randomMode[$_SESSION['mode']] == 'allow duplicates')
+	{
+		$civs = randDupCiv();
+	}
 
-$maps = ['Earth', 'continents', 'pangea', 'archipelago', 'fractal'];
-echo 'Map type: ', $maps[rand(0,4)], '<br/>';
+	echo 'Mode: ', $randomMode[$_SESSION['mode'] ], '<br/>';
+	echo '<a href="randomGame.php">Reroll</a><br/><br/>';
 
-$paces = ['quick', 'standard', 'epic', 'marathon'];
-echo 'Game pace: ', $paces[rand(0,3)];
+	$player = 1;
+
+	foreach($civs as $key => $value)
+	{
+		$results = database::fetchRows('civilization', 'name', 'id', $value);
+		$resultsRow = $results->fetch_row();
+		echo 'Player ', $player++, ': ', $resultsRow[0], '<br/>';
+	}
+
+	$victories = ['time', 'domination', 'science', 'diplomatic', 'culture'];
+	echo 'Victory type: ', $victories[rand(0,4)], ' victory<br/>';
+
+	$difficulties = ['settler', 'chieftain', 'warlord', 'prince', 'king', 'emperor', 'immortal', 'deity'];
+	echo 'Difficulty level: ', $difficulties[rand(0,7)], '<br/>';
+
+	$maps = ['Earth', 'continents', 'pangea', 'archipelago', 'fractal'];
+	echo 'Map type: ', $maps[rand(0,4)], '<br/>';
+
+	$paces = ['quick', 'standard', 'epic', 'marathon'];
+	echo 'Game pace: ', $paces[rand(0,3)];
+}
+else
+{
+	echo 'Please select a mode.';
+}
 ?>
